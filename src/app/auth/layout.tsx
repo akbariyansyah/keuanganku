ef34064194fac5d9f298a01718122a8147e4f6a5
Ryan <buchoriakbar21366@gmail.com>
2025-09-17 22:03:20 +0700

    chore: fix hydration error

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,13 +1,10 @@
-// app/(auth)/layout.tsx
-"use client";
+// Server Component
 import "@/app/globals.css";
 
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <html lang="en">
-            <body>
-                {children}
-            </body>
-        </html>
-    );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
