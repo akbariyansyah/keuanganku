7ab06f65c3d66d0d193713708ead08a42f4344a4
Ryan <buchoriakbar21366@gmail.com>
2025-09-21 19:42:19 +0700

    feat: enhance header page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,13 @@
+import { useMe } from "@/hooks/use-me";
 import { getGreeting } from "@/utils/greeting";
 
 export default function Header() {
     const greeting :string = getGreeting();
+    const { data: user } = useMe();
 
   return (
     <header className="w-full border-b bg-white p-4 m-5">
-      <h1 className="text-2xl font-bold">Hi, There ! </h1>
-      <h3 className="mt-7"> {greeting}</h3>
+      <h1 className="text-2xl font-bold"> Hi {user?.fullname} , {greeting} 👋</h1>
     </header>
   );
 }
