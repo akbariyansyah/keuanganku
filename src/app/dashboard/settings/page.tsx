3af92dafbd0f6e6c71f500ce52f1c74d00868de9
Ryan <buchoriakbar21366@gmail.com>
2025-09-22 21:47:03 +0700

    feat: add select currency on page

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,5 +1,27 @@
+"use client"
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectGroup, SelectLabel, SelectItem } from "@/components/ui/select";
+
 export default function SettingPage() {
-    return <>   
-    <h1> Manage your setting here</h1>
+    return <>
+        <div className="m-4">
+            <h1 className="text-2xl font-bold mb-4">Settings</h1>
+            <div className="flex flex-row gap-4 max-w-sm">
+                <p className="text-l mt-1">Currency </p>
+                <Select>
+                    <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="Select a Currency" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectLabel>Currency</SelectLabel>
+                            <SelectItem value="idr">IDR</SelectItem>
+                            <SelectItem value="usd">USD</SelectItem>
+
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+            </div>
+        </div>
+
     </>
 }
