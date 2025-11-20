55d1aca08c4de865a1beda0f9a98dcf1f7b8a9fc
Ryan <buchoriakbar21366@gmail.com>
2025-11-20 21:24:37 +0700

    feat: display investment menu based on toggle

--- src/constant/app-menu.ts
+++ src/constant/app-menu.ts
@@ -8,55 +8,63 @@ type NavItem = {
     children?: NavItem[];
 }
 
-export const sideBarList: NavItem[] = [
-    {
-        labelKey: "dashboard",
-        url: "/dashboard",
-        icon: Home,
-    },
-    {
-        labelKey: "transactions",
-        url: "/dashboard/transaction",
-        icon: Table,
-    },
-    {
-        labelKey: "investment",
-        url: "/dashboard/investment",
-        icon: DollarSignIcon,
-        children: [
-            {
-                labelKey: "investmentCategories",
-                url: "/dashboard/investment/categories",
-                icon: TableProperties,
-            },
-            {
-                labelKey: "investmentPortfolio",
-                url: "/dashboard/investment/portfolio",
-                icon: Wallet,
-                children: [
-                    {
-                        labelKey: "investmentPortfolioAdd",
-                        url: "/dashboard/investment/portfolio/add",
-                        icon: Plus,
-                    }
-                ]
-            },
-            {
-                labelKey: "investmentPerformance",
-                url: "/dashboard/investment/performance",
-                icon: TrendingUp,
-            },
+const showInvestmentFeature = process.env.NEXT_PUBLIC_SHOW_INVESTMENT_FEATURE === "true";
 
-        ]
-    },
-    {
-        labelKey: "journal",
-        url: "/dashboard/journal",
-        icon: NotebookPen,
-    },
-    {
-        labelKey: "settings",
-        url: "/dashboard/settings",
-        icon: Settings,
-    },
-]
+export const sideBarList = (): NavItem[] => {
+    const items: NavItem[] = [
+        {
+            labelKey: "dashboard",
+            url: "/dashboard",
+            icon: Home,
+        },
+        {
+            labelKey: "transactions",
+            url: "/dashboard/transaction",
+            icon: Table,
+        },
+        {
+            labelKey: "settings",
+            url: "/dashboard/settings",
+            icon: Settings,
+        },
+    ]
+
+    if (showInvestmentFeature) {
+        items.push({
+            labelKey: "investment",
+            url: "/dashboard/investment",
+            icon: DollarSignIcon,
+            children: [
+                {
+                    labelKey: "investmentCategories",
+                    url: "/dashboard/investment/categories",
+                    icon: TableProperties,
+                },
+                {
+                    labelKey: "investmentPortfolio",
+                    url: "/dashboard/investment/portfolio",
+                    icon: Wallet,
+                    children: [
+                        {
+                            labelKey: "investmentPortfolioAdd",
+                            url: "/dashboard/investment/portfolio/add",
+                            icon: Plus,
+                        }
+                    ]
+                },
+                {
+                    labelKey: "investmentPerformance",
+                    url: "/dashboard/investment/performance",
+                    icon: TrendingUp,
+                },
+
+            ]
+        }, 
+        {
+            labelKey: "journal",
+            url: "/dashboard/journal",
+            icon: NotebookPen,
+        },)
+    }
+    return items;
+}
