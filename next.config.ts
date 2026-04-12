9f55d219cca59786cd0b0f59f808e7fccea5af46
Ryan <buchoriakbar21366@gmail.com>
2026-04-12 21:01:17 +0700

    chore: remove unused config next

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
-import type { NextConfig } from 'next';
 
 // next.config.mjs or next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     // ⚠️ Danger: this lets builds pass even with TS errors
     ignoreBuildErrors: true,
