8b314f5c3e4552f21893644904509384aa95709d
Buchori Akbariyansyah <riyansean21366@gmail.com>
2026-05-02 17:17:59 +0700

    feat: slicing history investment page

--- src/app/dashboard/investment/performance/page.tsx
+++ src/app/dashboard/investment/performance/page.tsx
@@ -1,7 +1,30 @@
 'use client';
 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import HistoryInvestmentPageTable from '@/section/investment/performance/history-page-table';
 import PerformanceChartPage from '@/section/investment/performance/performance-chart';
+import { AppWindowIcon, TableIcon } from 'lucide-react';
 
 export default function ChartAreaInteractive() {
-  return <PerformanceChartPage />;
+  return <div className='px-2'>
+    <Tabs defaultValue="preview">
+      <TabsList>
+        <TabsTrigger value="preview">
+          <AppWindowIcon />
+          Summary
+        </TabsTrigger>
+        <TabsTrigger value="code">
+          <TableIcon />
+          History
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent value="preview">
+        <PerformanceChartPage />
+      </TabsContent>
+      <TabsContent value="code">
+        <HistoryInvestmentPageTable />
+      </TabsContent>
+    </Tabs>
+  </div>
+    ;
 }
---
+++ src/section/investment/performance/history-page-table.tsx
@@ -0,0 +1,51 @@
+import MetricCard, { MetricItem } from "@/components/common/metric-card";
+import { useMemo } from "react";
+import { TableHistoryInvestment } from "./table-history";
+
+
+
+export default function HistoryInvestmentPageTable() {
+
+    const items = useMemo(() => {
+        return [
+            {
+                title: 'Total withdrawn',
+                value: "12121212",
+            },
+            {
+                title: 'Number of withdrawals',
+                value: "23",
+            },
+            {
+                title: 'Last withdrawal',
+                value: "23",
+            },
+        ] satisfies Array<MetricItem>;
+    }, []);
+
+    return (
+        <div>
+            <div className="p-2">
+                <h3 className="text-xl font-bold">
+                    History Page
+                </h3>
+            </div>
+
+            {/* Metrics Cards */}
+            <div className="grid w-full gap-2 my-6 grid-cols-1 sm:grid-cols-2 xl:grid-cols-5">
+                {items.map((item, idx) => {
+                    return (
+                        <MetricCard
+                            key={idx}
+                            title={item.title}
+                            value={item.value}
+                        />
+                    )
+                })}
+            </div>
+
+            {/* Table */}
+            <TableHistoryInvestment/>
+        </div>
+    )
+}
